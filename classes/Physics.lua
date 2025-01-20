f458036f4d84c698f2f9df97a60a3e537b7f0ea6
fellipense <kingzinhu@hotmail.com>
2025-01-19 22:11:12 -0300

    add player (square) moving

--- classes/Physics.lua
+++ classes/Physics.lua
@@ -1,3 +1,15 @@
 function checkCollision(a, b)
     
+    if a.type == "rectangle" and b.type == "rectangle" then
+    
+        if a.x + a.width > b.x
+        and a.x < b.x + b.width
+        and a.y + a.height > b.y
+        and a.y < b.y + b.height
+            then return true
+            else return false
+        end
+
+    end
+
 end
--- main.lua
+++ main.lua
@@ -3,23 +3,38 @@ require("classes/Circle")
 tick = require "libs/tick"
 audio1 = love.audio.newSource("audio/C418-Aria_Math.mp3", "stream")
 shapes = {}
+player = {}
 
 function love.load()
 	love.audio.play(audio1)
 	love.graphics.setColor(0.5, 0.5, 1)
 	love.window.setTitle("serene")
+	player = newRectangle(0, 0, 100, 100)
+	table.insert(shapes, player)
+	player.speed = 200
 end
 
 function love.update(deltaTime)
 	tick.update(deltaTime)
+
+	if love.keyboard.isDown("a") then
+		player.x = player.x - player.speed * deltaTime
+	end
+	if love.keyboard.isDown("w") then
+		player.y = player.y - player.speed * deltaTime
+	end
+	if love.keyboard.isDown("s") then
+		player.y = player.y + player.speed * deltaTime
+	end
+	if love.keyboard.isDown("d") then
+		player.x = player.x + player.speed * deltaTime
+	end
 end
 
 function love.draw()
 	for i,s in ipairs(shapes) do
 		s:draw()
 	end
-
-	love.graphics.print(love.graphics.getWidth())
 end
 
 function love.keypressed(key)
